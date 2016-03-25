86522e8a64ff9faf0629f4bcfe14e2c5cec40a03
Yaniv Pascal <yanivpas@gmail.com>
2016-03-26 01:45:31 +0300

    A draft of the core code

--- machine_learning.py
+++ machine_learning.py
@@ -1,4 +1,8 @@
 from polynomial import Polynomial
+from matrix import Matrix, QRdecomposition, solveQR
+
+MAX_DEGREE = 20
+EPSILON = 0.0000001
 
 def squard_loss(h, point):
     (x, y) = point
@@ -12,13 +16,34 @@ def mean_squared_err(h, sample):
     return result/len(sample)
 
 
-class LinearMachine(object):
-    def __init__(self, sample):
-        self.sample = sample
-        # TODO: check that all the vectors in the sample are in the same dimentation
-        # FIXME:
-        self.dim = len(sample[0][0])
+
 
 class PolyMachine(object):
     def __init__(self, sample):
-        pass
+        self.sample = sample
+
+    def polynomial(self, degree):
+        raws = []
+        vector = []
+        for (x, y) in self.sample:
+            raws.append([x**i for i in range(0, degree)])
+            vector.append(y)
+
+        A = Matrix(raws)
+        (Q, R) = QRdecomposition(A)
+        result = solveQR(Q, R, vector)
+        return Polynomial(result)
+
+    def run(self):
+        polynomials = []
+        for degree in range(1, MAX_DEGREE):
+            p = self.polynomial(degree)
+            polynomials.append(p)
+            print "polynomial: ", p.vector, " mean:", mean_squared_err(p, self.sample)
+            if mean_squared_err(p, self.sample) < EPSILON:
+                break
+
+        return p
+
+        
+
